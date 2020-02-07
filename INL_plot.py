f18c44d4fa73523bd39f9c507d98716173596334
Azzzrail <azzzrail@bk.ru>
2020-02-08 00:18:28 +0300

    Optimization done, comments added

--- INL_plot.py
+++ INL_plot.py
@@ -1,21 +1,18 @@
 # coding=utf-8
-from typing import List
+import typing
 import glob
 import os
 import sys
 import numpy as np
 
-
 from bokeh.plotting import figure, output_file, show
 import itertools  # Для перебора цветов в графиках
-from bokeh.palettes import Dark2_5 as palette # Палитра цветов для линий
-
+from bokeh.palettes import Dark2_5 as Palette  # Палитра цветов для линий
 
 import pandas as pd
 import os
 import sys
-#import warnings
-#warnings.filterwarnings("ignore", 'This pattern has match groups')
+
 
 def make_paus(mode, message=""):
     if mode == 0:
@@ -29,88 +26,67 @@ def make_paus(mode, message=""):
 
 Channel = int(make_paus(0, "enter channel number"))
 
-print(Channel)
-
-i = 0
+i: int = 0
 line: str
-filenames: List[str] = []
+filenames: typing.List[str] = []
 df = {}
 new_cols = {}
-path = os.getcwd()
-for filename in glob.iglob(path + '/**/TDC72VXS*.*', recursive=True):
+path = os.getcwd()  # получаем путь до директории, где лежит скрипт и рядом лежат файлы данных
+for filename in glob.iglob(path + '/**/TDC72VXS*.*', recursive=True):  # рекурсивно перебираем файлы в папках по шаблону
     i += 1
-    #if filename not in filenames:
-    filenames.insert(i, filename)
+    # if filename not in filenames:
+    filenames.insert(i, filename)  # И записываем имена файлов в список
 
 j: int = 0
-i: int = 0
+i = 0
 
 with open('filename.txt', "w") as g:
     for item in filenames:
-        g.write("%s\n" % item)
+        g.write("%s\n" % item)  # Записываем имена файлов в текстовый файл
 
 while j < len(filenames):
-
-    df[j] = pd.read_csv(filenames[j], skipinitialspace=True, skiprows=1,  header=None, sep='[_]|[=]|[,]|\s+]', engine='python')
+    df[j] = pd.read_csv(filenames[j], skipinitialspace=True, skiprows=1, header=None, sep='[_]|[=]|[,]|s+]',
+                        engine='python')  # Вычитываем файлы в датафрейм, пропуская первую строку, без имён столбцов
+    # Разделители подобраны по тому, что есть в файле. Возможно, надо запилить ручной ввод разделителей.
     # get length of df's columns
-    num_cols = len(list(df[j]))
-
-    # generate range of ints for suffixes
-    # with length exactly half that of num_cols;
-    # if num_cols is even, truncate concatenated list later
-    # to get to original list length
-    new_cols = list(['Ch'] + [i for i in range(0, num_cols-1)])
-    df[j].columns = new_cols
-
-    df[j] = df[j][df[j].Ch != 'stat']
-
-    df[j]['Ch'] = pd.to_numeric(df[j]['Ch'])
-    df[j] = df[j].set_index('Ch')
-    df[j].index.names = [None]
-
-#        df[j].index.astype(int)
-#        df[j] = df[j].astype(float)
-
+    num_cols = 1025  # len(list(df[j])) Я знаю размер таблицы, поэтому число, но можно вычислить автоматом. Но это не
+    # оптимальное решение
+    df[j] = df[j].dropna()  # выкидываем строки с NaN в элементах
+    df[j].columns = list([i for i in range(0, num_cols)])  # Создаю имена столцов - список с числами от 0 до 1025
+    df[j] = df[j].set_index(0)  # Запихиваем нулевой столбец(номера каналов в файле) в индексы
+    df[j].index = pd.to_numeric(df[j].index)  # Превращаем индексы из объектов в числа, чтобы можно было их итерировать
+    df[j].index.names = [None]  # удаляем имя для индексов(там был 0 - номер столбца)
     j += 1
 
-#print(filenames[1])
-#df[Channel].loc[[0]].plot(legend=True) #plot usa column
-
-t = 0
-names = []
-while t < len(filenames):
-    name = os.path.basename(filenames[t])
-    names.insert(t, name)
-    t += 1
-
-#print("amount of names", names)
-
-#x: List[int] = []
 i = 0
-j: int = 0
-Max_line = int(0)
+names = []
+while i < len(filenames):
+    name = os.path.basename(filenames[i])  # Вычленяем имя файла из полного пути к нему
+    names.insert(i, name)  # Записываем в список, потом будем имена использовать в качестве легенды к линиям в плоте
+    i += 1
 
-output_file("log_lines.html")
+output_file("log_lines.html")  # Создаём выходной файл с плотом
 # набор инструментов для работы с нарисованным графиком
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
 # create a new plot
 p = figure(
-   tools=TOOLS, width=1500, height=1000,
-   y_axis_type="linear",  title="TDC72_INL",
-   x_axis_label='buf_place', y_axis_label='counts'
-          )
+    tools=TOOLS, width=1500, height=1000,
+    y_axis_type="linear", title="TDC72_INL",
+    x_axis_label='buf_place', y_axis_label='counts'
+)
 
-colors = itertools.cycle(palette)
+i = 0
+j = 0
+Max_line = int(0)
+colors = itertools.cycle(Palette)
 
 for Max_line, color in zip(range(0, len(names)), colors):
 
-    try:
-        p.line(x=range(1024), y=[i for i in df[Max_line].loc[(Channel)]],
-               legend_label=names[Max_line], color=color)
-    except KeyError:
-        continue
+    try:  # Пытаемся нарисовать выбранный канал, итерируя датафреймы(файлы)
+        output_file = p.line(x=range(1024), y=[i for i in df[Max_line].loc[Channel]],
+                             legend_label=names[Max_line], color=color)
+    except KeyError:  # В случае появления номера канала, который отсутствует в таблице, обрабатываем возможную ошибку
+        continue  # Делаем нихера, просто пропуская эту итерацию и ждём следующей.
+        #  В случае, если нет ни одного канала в данных, получаем пустой плот
 
 show(p)
-
-
-
