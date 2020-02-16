c158e4c0463f98699dad74e07e1d79c8ae4e2443
Azzzrail <azzzrail@bk.ru>
2020-02-16 23:14:41 +0300

    Checkbox added, but without reading

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
-ch: int
 
+ch: int
 
 def draw_gui():
     global ch
@@ -8,8 +8,16 @@ def draw_gui():
     # All the stuff inside your window.
     layout = [[sg.Text('Table plotter v0.32')],
               [sg.Text('Enter channel number'), sg.InputText()],
+              [sg.Text('Plot tools enable/disable')],
+              # чербоксы для работы с нарисованной гистограммой
+              [sg.Checkbox('pan', default=True), sg.Checkbox('wheel_zoom', default=True),
+               sg.Checkbox('box_zoom', default=True), sg.Checkbox('reset', default=True),
+               sg.Checkbox('save', default=True), sg.Checkbox('box_select', default=True)],
+              [sg.Checkbox('lasso_select', default=True),
+               sg.Checkbox('crosshair', default=True), sg.Checkbox('hover', default=True)],
+              # Конец чекбоксов
               [sg.Button('Ok'), sg.Button('Cancel')]]
-
+# TODO: Добавить вычитку состояний чекбоксов
     # Create the Window
     window = sg.Window('Window Title', layout)
     # Event Loop to process "events" and get the "values" of the inputs
@@ -22,4 +30,7 @@ def draw_gui():
             break
 
     window.close()
-    return ch
+    try:
+        return ch
+    except NameError:
+        return None
